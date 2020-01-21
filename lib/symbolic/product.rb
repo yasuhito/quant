672cea508114aa616a1fd254723f7146b20c11de
Yasuhito Takamiya <yasuhito@gmail.com>
2020-01-21 16:04:23 +0900

    積の simplification を追加

--- lib/symbolic/product.rb
+++ lib/symbolic/product.rb
@@ -12,6 +12,31 @@ module Symbolic
         @elements[0]
       elsif @elements.any? { |each| each.is_a?(Numeric) && each.zero? }
         0
+      else
+        simplify_rec_result = simplify_rec(*@elements)
+        if simplify_rec_result.size == 1
+          simplify_rec_result[0]
+        end
+      end
+    end
+
+    private
+
+    def simplify_rec(*elements)
+      if elements.size == 2 && elements[0].is_a?(Numeric) && elements[1].is_a?(Numeric)
+        list_or_null_if_1 elements[0] * elements[1]
+      elsif elements.size == 2 && elements[0] == 1
+        [elements[1]]
+      elsif elements.size == 2 && elements[1] == 1
+        [elements[0]]
+      end
+    end
+
+    def list_or_null_if_1(x)
+      if x == 1
+        []
+      else
+        [x]
       end
     end
   end
--- test/symbolic/product_test.rb
+++ test/symbolic/product_test.rb
@@ -17,5 +17,23 @@ module Symbolic
 
       assert_equal 0, prod
     end
+
+    test 'Prod(Num1, Num2) = Num1 * Num2' do
+      prod = Prod(2, 3)
+
+      assert_equal 6, prod
+    end
+
+    test 'Prod(1, x) = x' do
+      prod = Prod(1, :x)
+
+      assert_equal :x, prod
+    end
+
+    test 'Prod(x, 1) = x' do
+      prod = Prod(:x, 1)
+
+      assert_equal :x, prod
+    end
   end
 end
