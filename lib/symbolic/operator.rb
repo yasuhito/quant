6158e02c96e7a204b5efc924a7952e2ea9af5d67
Yasuhito Takamiya <yasuhito@gmail.com>
2020-01-28 10:34:33 +0900

    exponent オペレータを追加

--- lib/symbolic/operator.rb
+++ lib/symbolic/operator.rb
@@ -5,7 +5,7 @@ module Symbolic
 
   # Symbolic computation operators
   module Operator
-    # Returns the base of an ASAE (automatically simplified algebraic expression)
+    # Returns the base of an ASAE
     #
     # base(u) = | u  when u is a symbol, product, sum, factorial, or function,
     #           | operand(u, 1)  when u is a power,
@@ -22,6 +22,28 @@ module Symbolic
       return u if u.is_a?(Array) && u[0] == :f
 
       return operand(u, 1) if u.is_a?(Array) && u[0] == :^
+
+      return UNDEFINED if u.is_a?(Integer)
+      return UNDEFINED if u.is_a?(Array) && u[0] == :fraction
+    end
+
+    # Returns the exponent of an ASAE
+    #
+    # exponent(u) = | 1  when u is a symbol, product, sum, factorial, or function,
+    #               | operand(u, 2)  when u is a power,
+    #               | Undefined  when u is an integer or fraction
+    #
+    # Examples:
+    #   exponent(x^2) = 2
+    #   exponent(x) = 1
+    def exponent(u)
+      return 1 if u.is_a?(Symbol)
+      return 1 if u.is_a?(Array) && u[0] == :*
+      return 1 if u.is_a?(Array) && u[0] == :+
+      return 1 if u.is_a?(Array) && u[0] == :!
+      return 1 if u.is_a?(Array) && u[0] == :f
+
+      return operand(u, 2) if u.is_a?(Array) && u[0] == :^
 
       return UNDEFINED if u.is_a?(Integer)
       return UNDEFINED if u.is_a?(Array) && u[0] == :fraction
--- test/symbolic/operator_test.rb
+++ test/symbolic/operator_test.rb
@@ -5,39 +5,77 @@ require 'test_helper'
 require 'symbolic/operator'
 
 module Symbolic
-  class OperatorTest < ActiveSupport::TestCase
-    include Operator
+  module Operator
+    class BaseTest < ActiveSupport::TestCase
+      include Operator
 
-    test 'base(u) = u when u is a symbol' do
-      assert_equal :x, base(:x)
+      test 'base(u) = u when u is a symbol' do
+        assert_equal :x, base(:x)
+      end
+
+      test 'base(u) = u when u is a product' do
+        assert_equal %i[* x y], base(%i[* x y])
+      end
+
+      test 'base(u) = u when u is a sum' do
+        assert_equal %i[+ x y], base(%i[+ x y])
+      end
+
+      test 'base(u) = u when u is a factorial' do
+        assert_equal %i[! x], base(%i[! x])
+      end
+
+      test 'base(u) = u when u is a function' do
+        assert_equal %i[f f x], base(%i[f f x])
+      end
+
+      test 'base(u) = operand(u, 1) when u is a power' do
+        assert_equal :x, base([:^, :x, 2])
+      end
+
+      test 'base(u) = Undefined when u is an integer' do
+        assert_equal :Undefined, base(1)
+      end
+
+      test 'base(u) = Undefined when u is a fraction' do
+        assert_equal :Undefined, base([:fraction, 1, 3])
+      end
     end
 
-    test 'base(u) = u when u is a product' do
-      assert_equal %i[* x y], base(%i[* x y])
-    end
+    class ExponentTest < ActiveSupport::TestCase
+      include Operator
 
-    test 'base(u) = u when u is a sum' do
-      assert_equal %i[+ x y], base(%i[+ x y])
-    end
+      test 'exponent(u) = 1 when u is a symbol' do
+        assert_equal 1, exponent(:x)
+      end
 
-    test 'base(u) = u when u is a factorial' do
-      assert_equal %i[! x], base(%i[! x])
-    end
+      test 'exponent(u) = 1 when u is a product' do
+        assert_equal 1, exponent(%i[* x y])
+      end
 
-    test 'base(u) = u when u is a function' do
-      assert_equal %i[f f x], base(%i[f f x])
-    end
+      test 'exponent(u) = 1 when u is a sum' do
+        assert_equal 1, exponent(%i[+ x y])
+      end
 
-    test 'base(u) = operand(u, 1) when u is a power' do
-      assert_equal :x, base([:^, :x, 2])
-    end
+      test 'exponent(u) = 1 when u is a factorial' do
+        assert_equal 1, exponent(%i[! x])
+      end
 
-    test 'base(u) = Undefined when u is an integer' do
-      assert_equal :Undefined, base(1)
-    end
+      test 'exponent(u) = 1 when u is a function' do
+        assert_equal 1, exponent(%i[f f x])
+      end
 
-    test 'base(u) = Undefined when u is a fraction' do
-      assert_equal :Undefined, base([:fraction, 1, 3])
+      test 'exponent(u) = operand(u, 2) when u is a power' do
+        assert_equal 2, exponent([:^, :x, 2])
+      end
+
+      test 'exponent(u) = Undefined when u is an integer' do
+        assert_equal :Undefined, exponent(1)
+      end
+
+      test 'exponent(u) = Undefined when u is a fraction' do
+        assert_equal :Undefined, exponent([:fraction, 1, 3])
+      end
     end
   end
 end
