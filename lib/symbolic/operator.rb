ff8548e180400ada2b489e9a6d895642dcb872f6
Yasuhito Takamiya <yasuhito@gmail.com>
2020-01-29 09:22:26 +0900

    const オペレータを追加

--- lib/symbolic/operator.rb
+++ lib/symbolic/operator.rb
@@ -51,7 +51,7 @@ module Symbolic
 
     # Returns the like-term part of an ASAE
     #
-    # term(u) = | ・u when u is a symbol, sum, power, factorial, or function,
+    # term(u) = | ·u when u is a symbol, sum, power, factorial, or function,
     #           | u2···un when u = u1···un is a product and u1 is constant,
     #           | u when when u = u1 ··· un is a product and u1 is not constant,
     #           | Undefined when u is an integer or fraction
@@ -66,6 +66,28 @@ module Symbolic
       return [:*, *u[2..-1]] if u.is_a?(Array) && u[0] == :* && u[1].is_a?(Array) && u[1][0] == :fraction
 
       return u if u.is_a?(Array) && u[0] == :* && !(u[1].is_a?(Integer) || (u[1].is_a?(Array) && u[1][0] == :fraction))
+
+      return UNDEFINED if u.is_a?(Integer)
+      return UNDEFINED if u.is_a?(Array) && u[0] == :fraction
+    end
+
+    # Returns the const part of an ASAE
+    #
+    # const(u) = | 1 when u is a symbol, sum, power, factorial, or function,
+    #            | u1 when u = u1···un is a product and u1 is constant,
+    #            | 1 when u = u1···un is a product and u1 is not constant,
+    #            | Undefined when u is an integer or fraction
+    def const(u)
+      return 1 if u.is_a?(Symbol)
+      return 1 if u.is_a?(Array) && u[0] == :+
+      return 1 if u.is_a?(Array) && u[0] == :^
+      return 1 if u.is_a?(Array) && u[0] == :!
+      return 1 if u.is_a?(Array) && u[0] == :f
+
+      return u[1] if u.is_a?(Array) && u[0] == :* && u[1].is_a?(Integer)
+      return u[1] if u.is_a?(Array) && u[0] == :* && u[1].is_a?(Array) && u[1][0] == :fraction
+
+      return 1 if u.is_a?(Array) && u[0] == :* && !(u[1].is_a?(Integer) || (u[1].is_a?(Array) && u[1][0] == :fraction))
 
       return UNDEFINED if u.is_a?(Integer)
       return UNDEFINED if u.is_a?(Array) && u[0] == :fraction
--- test/symbolic/operator_test.rb
+++ test/symbolic/operator_test.rb
@@ -81,23 +81,23 @@ module Symbolic
     class TermTest < ActiveSupport::TestCase
       include Operator
 
-      test 'term(u) = ・u when u is a symbol' do
+      test 'term(u) = ·u when u is a symbol' do
         assert_equal %i[* x], term(:x)
       end
 
-      test 'term(u) = ・u when u is a sum' do
+      test 'term(u) = ·u when u is a sum' do
         assert_equal [:*, %i[+ x y]], term(%i[+ x y])
       end
 
-      test 'term(u) = ・u when u is a power' do
+      test 'term(u) = ·u when u is a power' do
         assert_equal [:*, [:^, :x, 2]], term([:^, :x, 2])
       end
 
-      test 'term(u) = ・u when u is a factorial' do
+      test 'term(u) = ·u when u is a factorial' do
         assert_equal [:*, %i[! x]], term(%i[! x])
       end
 
-      test 'term(u) = ・u when u is a function' do
+      test 'term(u) = ·u when u is a function' do
         assert_equal [:*, %i[f f x]], term(%i[f f x])
       end
 
@@ -116,6 +116,47 @@ module Symbolic
 
       test 'term(u) = Undefined when u is a fraction' do
         assert_equal :Undefined, term([:fraction, 1, 3])
+      end
+    end
+
+    class ConstTest < ActiveSupport::TestCase
+      include Operator
+
+      test 'const(u) = 1 when u is a symbol' do
+        assert_equal 1, const(:x)
+      end
+
+      test 'const(u) = 1 when u is a sum' do
+        assert_equal 1, const(%i[+ x y])
+      end
+
+      test 'const(u) = 1 when u is a power' do
+        assert_equal 1, const([:^, :x, 2])
+      end
+
+      test 'const(u) = 1 when u is a factorial' do
+        assert_equal 1, const(%i[! x])
+      end
+
+      test 'const(u) = 1 when u is a function' do
+        assert_equal 1, const(%i[f f x])
+      end
+
+      test 'const(u) = u1 when u = u1···un is a product and u1 is constant' do
+        assert_equal 2, const([:*, 2, :x, :y, :z])
+        assert_equal [:fraction, 1, 3], const([:*, [:fraction, 1, 3], :x, :y, :z])
+      end
+
+      test 'const(u) = u1 when u = u1···un is a product and u1 is not constant' do
+        assert_equal 1, const(%i[* x y z])
+      end
+
+      test 'const(u) = Undefined when u is an integer' do
+        assert_equal :Undefined, const(1)
+      end
+
+      test 'const(u) = Undefined when u is a fraction' do
+        assert_equal :Undefined, const([:fraction, 1, 3])
       end
     end
   end
