d3f2f6966677592341ee1adfbed1e3fec5831e2d
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-05 16:00:08 +0900

    S ゲートを Symbo にする

--- test/s_gate_test.rb
+++ test/s_gate_test.rb
@@ -4,45 +4,45 @@ require 'test_helper'
 require 'circuit'
 
 class SGateTest < ActiveSupport::TestCase
-  # test 'S|0> = |0>' do
-  #   circuit = Circuit.new(1)
-  #   result = circuit.s(0)
+  test 'S|0> = |0>' do
+    circuit = Circuit.new(1)
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit[0]], result.state
-  # end
+    assert_equal [Qubit[0]], state
+  end
 
-  # test 'S|1> = i|1>' do
-  #   circuit = Circuit.new(Qubit[1])
-  #   result = circuit.s(0)
+  test 'S|1> = i|1>' do
+    circuit = Circuit.new(Qubit[1])
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit[1] * 1i], result.state
-  # end
+    assert_equal [Qubit[1] * 1i], state
+  end
 
-  # test 'S|+> = |i>' do
-  #   circuit = Circuit.new(Qubit['+'])
-  #   result = circuit.s(0)
+  test 'S|+> = |i>' do
+    circuit = Circuit.new(Qubit['+'])
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit['i']], result.state
-  # end
+    assert_equal [Qubit['i']], state
+  end
 
-  # test 'S|-> = |-i>' do
-  #   circuit = Circuit.new(Qubit['-'])
-  #   result = circuit.s(0)
+  test 'S|-> = |-i>' do
+    circuit = Circuit.new(Qubit['-'])
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit['-i']], result.state
-  # end
+    assert_equal [Qubit['-i']], state
+  end
 
-  # test 'S|i> = |->' do
-  #   circuit = Circuit.new(Qubit['i'])
-  #   result = circuit.s(0)
+  test 'S|i> = |->' do
+    circuit = Circuit.new(Qubit['i'])
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit['-']], result.state
-  # end
+    assert_equal [Qubit['-']], state
+  end
 
-  # test 'S|-i> = |+>' do
-  #   circuit = Circuit.new(Qubit['-i'])
-  #   result = circuit.s(0)
+  test 'S|-i> = |+>' do
+    circuit = Circuit.new(Qubit['-i'])
+    state = circuit.s(0).state
 
-  #   assert_equal [Qubit['+']], result.state
-  # end
+    assert_equal [Qubit['+']], state
+  end
 end
