7f5ed858bb9e2258a92a6c424366c14e1d9af1c4
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-13 12:00:25 +0900

    ケットの直交基底による書き直しを追加

--- test/quant/matrix_test.rb
+++ test/quant/matrix_test.rb
@@ -2,6 +2,8 @@
 
 module Quant
   class MatrixTest < ActiveSupport::TestCase
+    using Symbo
+
     test 'matrix multiplication' do
       a1 = Bra[1, -4, 2]
       a2 = Bra[2, 3, 0]
@@ -12,6 +14,16 @@ module Quant
       b = Matrix.columns([b1, b2])
 
       assert_equal Matrix[[-15, -16], [23, 19]], a * b
+    end
+
+    test 'rewrite |↑> in terms of an orthonormal basis |→> and <←|' do
+      a = Matrix.columns([Ket['→'], Ket['←']])
+      pamp = a.t * Ket['↑']
+
+      assert_equal 1, pamp.column_size
+      assert_equal 2, pamp.row_size
+      assert_equal 1/√(2), pamp[0, 0]
+      assert_equal 1/√(2), pamp[1, 0]
     end
   end
 end
