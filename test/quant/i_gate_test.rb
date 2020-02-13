f6f35c3db08d35edcc10bba0c6b9380ed672f59b
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-13 15:28:31 +0900

    テストを短くする

--- test/quant/i_gate_test.rb
+++ test/quant/i_gate_test.rb
@@ -8,44 +8,38 @@ module Quant
   class IGateTest < ActiveSupport::TestCase
     test 'I|0> = |0>' do
       circuit = Circuit.new(1)
-      state = circuit.i(0).state
 
-      assert_equal [Qubit[0]], state
+      assert_equal [Qubit[0]], circuit.i(0).state
     end
 
     test 'I|1> = |1>' do
       circuit = Circuit.new(Qubit[1])
-      state = circuit.i(0).state
 
-      assert_equal [Qubit[1]], state
+      assert_equal [Qubit[1]], circuit.i(0).state
     end
 
     test 'I|+> = |+>' do
       circuit = Circuit.new(Qubit['+'])
-      state = circuit.i(0).state
 
-      assert_equal [Qubit['+']], state
+      assert_equal [Qubit['+']], circuit.i(0).state
     end
 
     test 'I|-> = |->' do
       circuit = Circuit.new(Qubit['-'])
-      state = circuit.i(0).state
 
-      assert_equal [Qubit['-']], state
+      assert_equal [Qubit['-']], circuit.i(0).state
     end
 
     test 'I|i> = |i>' do
       circuit = Circuit.new(Qubit['i'])
-      state = circuit.i(0).state
 
-      assert_equal [Qubit['i']], state
+      assert_equal [Qubit['i']], circuit.i(0).state
     end
 
     test 'I|-i> = |-i>' do
       circuit = Circuit.new(Qubit['-i'])
-      state = circuit.i(0).state
 
-      assert_equal [Qubit['-i']], state
+      assert_equal [Qubit['-i']], circuit.i(0).state
     end
   end
 end
