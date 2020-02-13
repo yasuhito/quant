c7e9f4b048dae3d0c1751aabfce6121d417c53cd
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-13 15:49:21 +0900

    ZGate → Quant::ZGate に移動 (#9)

---
+++ test/quant/z_gate_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'quant/circuit'
+
+module Quant
+  class ZGateTest < ActiveSupport::TestCase
+    test 'Z|0> = |0>' do
+      circuit = Circuit.new(1)
+
+      assert_equal [Qubit[0]], circuit.z(0).state
+    end
+
+    test 'Z|1> = -|1>' do
+      circuit = Circuit.new(Qubit[1])
+
+      assert_equal [Qubit[1] * -1], circuit.z(0).state
+    end
+
+    test 'Z|+> = |->' do
+      circuit = Circuit.new(Qubit['+'])
+
+      assert_equal [Qubit['-']], circuit.z(0).state
+    end
+
+    test 'Z|-> = |+>' do
+      circuit = Circuit.new(Qubit['-'])
+
+      assert_equal [Qubit['+']], circuit.z(0).state
+    end
+
+    test 'Z|i> = |-i>' do
+      circuit = Circuit.new(Qubit['i'])
+
+      assert_equal [Qubit['-i']], circuit.z(0).state
+    end
+
+    test 'Z|-i> = |i>' do
+      circuit = Circuit.new(Qubit['-i'])
+
+      assert_equal [Qubit['i']], circuit.z(0).state
+    end
+  end
+end
