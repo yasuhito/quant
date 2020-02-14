52cde78411770e3998feef7f4cd579b0f602e966
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-14 17:36:30 +0900

    sin, cos の使われていないコードを削除

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter %r{^/test/}
+end
 
 require 'active_support'
 require 'active_support/test_case'
