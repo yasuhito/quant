ad04fb908d56f59d7c6e05e3e30c02f1ee9104b0
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-15 15:38:09 +0900

    Cos(x) → Cos[x] に API を変更

--- lib/quant/ry_gate.rb
+++ lib/quant/ry_gate.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
+require 'symbo/cos'
 require 'quant/gate'
 
 module Quant
   class RyGate < Gate
+    include Symbo
+
     def initialize(theta)
       @theta = theta
     end
 
     def matrix
-      Matrix[[Cos(@theta/2), -1 * Sin(@theta/2)],
-             [Sin(@theta/2), Cos(@theta/2)]]
+      Matrix[[Cos[@theta/2], -1 * Sin(@theta/2)],
+             [Sin(@theta/2), Cos[@theta/2]]]
     end
   end
 end
