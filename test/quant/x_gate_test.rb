a2bfc11650e361ae333df6c5b3ae911a193ec692
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-17 16:14:40 +0900

    X ゲートのテストを追加

--- test/quant/x_gate_test.rb
+++ test/quant/x_gate_test.rb
@@ -5,41 +5,51 @@ require 'test_helper'
 require 'quant/circuit'
 
 module Quant
-  class XGateTest < ActiveSupport::TestCase
-    test 'X|0> = |1>' do
-      circuit = Circuit.new(1)
+  class XGateTest
+    class BasicQubitTransitionTest < ActiveSupport::TestCase
+      test 'X|0> = |1>' do
+        circuit = Circuit.new(1)
 
-      assert_equal [Qubit[1]], circuit.x(0).state
+        assert_equal [Qubit[1]], circuit.x(0).state
+      end
+
+      test 'X|1> = |0>' do
+        circuit = Circuit.new(Qubit[1])
+
+        assert_equal [Qubit[0]], circuit.x(0).state
+      end
+
+      test 'X|+> = |+>' do
+        circuit = Circuit.new(Qubit['+'])
+
+        assert_equal [Qubit['+']], circuit.x(0).state
+      end
+
+      test 'X|-> = -|->' do
+        circuit = Circuit.new(Qubit['-'])
+
+        assert_equal [-Qubit['-']], circuit.x(0).state
+      end
+
+      test 'X|i> = i|-i>' do
+        circuit = Circuit.new(Qubit['i'])
+
+        assert_equal [1i * Qubit['-i']], circuit.x(0).state
+      end
+
+      test 'X|-i> = -i|i>' do
+        circuit = Circuit.new(Qubit['-i'])
+
+        assert_equal [-1i * Qubit['i']], circuit.x(0).state
+      end
     end
 
-    test 'X|1> = |0>' do
-      circuit = Circuit.new(Qubit[1])
+    class QuantumKatasTest < ActiveSupport::TestCase
+      test 'X gate flips qubit state' do
+        circuit = Circuit.new(Qubit[:α, :β])
 
-      assert_equal [Qubit[0]], circuit.x(0).state
-    end
-
-    test 'X|+> = |+>' do
-      circuit = Circuit.new(Qubit['+'])
-
-      assert_equal [Qubit['+']], circuit.x(0).state
-    end
-
-    test 'X|-> = -|->' do
-      circuit = Circuit.new(Qubit['-'])
-
-      assert_equal [-Qubit['-']], circuit.x(0).state
-    end
-
-    test 'X|i> = i|-i>' do
-      circuit = Circuit.new(Qubit['i'])
-
-      assert_equal [1i * Qubit['-i']], circuit.x(0).state
-    end
-
-    test 'X|-i> = -i|i>' do
-      circuit = Circuit.new(Qubit['-i'])
-
-      assert_equal [-1i * Qubit['i']], circuit.x(0).state
+        assert_equal [Qubit[:β, :α]], circuit.x(0).state
+      end
     end
   end
 end
