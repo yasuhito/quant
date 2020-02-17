a33378ba857bb6c8577a1e8d8dcfb780bfc5878d
Yasuhito Takamiya <yasuhito@gmail.com>
2020-02-17 11:09:58 +0900

    Quot(...) → Quot[...] に API 変更

--- test/symbo/quot_test.rb
+++ test/symbo/quot_test.rb
@@ -8,16 +8,16 @@ module Symbo
   class QuotTest < ActiveSupport::TestCase
     using Symbo
 
-    test 'Quot(1, 0).evaluate → Undefined' do
-      assert_equal UNDEFINED, Quot(1, 0).evaluate
+    test 'Quot[1, 0].evaluate → Undefined' do
+      assert_equal UNDEFINED, Quot[1, 0].evaluate
     end
 
-    test 'Quot(1, 2).evaluate → 1/2' do
-      assert_equal 1/2, Quot(1, 2).evaluate
+    test 'Quot[1, 2].evaluate → 1/2' do
+      assert_equal 1/2, Quot[1, 2].evaluate
     end
 
-    test 'Quot(2/3, 4/5).evaluate → 10/12' do
-      assert_equal 10/12, Quot(2/3, 4/5).evaluate
+    test 'Quot[2/3, 4/5].evaluate → 10/12' do
+      assert_equal 10/12, Quot[2/3, 4/5].evaluate
     end
   end
 end
